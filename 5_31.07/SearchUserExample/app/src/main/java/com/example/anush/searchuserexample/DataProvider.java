4208f254bad3cf257958c0073012569e00993579
Anush_Ghazaryan <anush93ghazaryan@gmail.com>
2018-07-31 23:43:24 +0400

    Modified

--- 5_31.07/SearchUserExample/app/src/main/java/com/example/anush/searchuserexample/DataProvider.java
+++ 5_31.07/SearchUserExample/app/src/main/java/com/example/anush/searchuserexample/DataProvider.java
@@ -17,6 +17,28 @@ public class DataProvider {
                 "https://media.newyorker.com/photos/59097fa9c14b3c606c109c9e/4:3/w_960,c_limit/Petrusich-John-Mayer.jpg"));
         users.add(new User("Meghan","Actress","tel:043919089","aram@gmail.com",
                 "https://s3.r29static.com//bin/entry/4c8/340x408,80/1896700/image.jpg"));
+        users.add(new User("Hilary","Actress","tel:+18185464608","hilary@gmail.com",
+                "https://vignette.wikia.nocookie.net/gossipgirl/images/6/67/Hilary-duff.jpg/revision/latest/scale-to-width-down/350?cb=20161014121528"));
+
+        users.add(new User("Adam","Doctor","tel:043919089","adam@gmail.com",
+                "https://i.pinimg.com/736x/77/3c/51/773c51fa4439756303a9dfeac48d789a--mac-google-search.jpg"));
+
+        users.add(new User("John","Doctor","tel:043919089","john@gmail.com",
+                "https://media.newyorker.com/photos/59097fa9c14b3c606c109c9e/4:3/w_960,c_limit/Petrusich-John-Mayer.jpg"));
+        users.add(new User("Meghan","Actress","tel:043919089","aram@gmail.com",
+                "https://s3.r29static.com//bin/entry/4c8/340x408,80/1896700/image.jpg"));
+
+        users.add(new User("Hilary","Actress","tel:+18185464608","hilary@gmail.com",
+                "https://vignette.wikia.nocookie.net/gossipgirl/images/6/67/Hilary-duff.jpg/revision/latest/scale-to-width-down/350?cb=20161014121528"));
+
+        users.add(new User("Adam","Doctor","tel:043919089","adam@gmail.com",
+                "https://i.pinimg.com/736x/77/3c/51/773c51fa4439756303a9dfeac48d789a--mac-google-search.jpg"));
+
+        users.add(new User("John","Doctor","tel:043919089","john@gmail.com",
+                "https://media.newyorker.com/photos/59097fa9c14b3c606c109c9e/4:3/w_960,c_limit/Petrusich-John-Mayer.jpg"));
+        users.add(new User("Meghan","Actress","tel:043919089","aram@gmail.com",
+                "https://s3.r29static.com//bin/entry/4c8/340x408,80/1896700/image.jpg"));
+
 
 
     }
