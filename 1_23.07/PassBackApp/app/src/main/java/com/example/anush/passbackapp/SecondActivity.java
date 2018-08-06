955697997e3194b01e2f185bf591b5c0766ddada
Anush_Ghazaryan <anush93ghazaryan@gmail.com>
2018-08-07 03:33:06 +0400

    Added PassData Task

---
+++ 1_23.07/PassBackApp/app/src/main/java/com/example/anush/passbackapp/SecondActivity.java
@@ -0,0 +1,29 @@
+package com.example.anush.passbackapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        final EditText etData = findViewById(R.id.et_modify);
+
+        final Button btnModify = findViewById(R.id.btn_modify);
+        etData.setText(getIntent().getStringExtra(MainActivity.DATA_KEY));
+        btnModify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra(MainActivity.DATA_KEY, etData.getText().toString());
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
