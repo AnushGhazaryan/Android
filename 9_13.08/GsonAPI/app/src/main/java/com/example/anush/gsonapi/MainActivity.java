1862cbdb7c7648b4d57967e836ffc224873451a9
Anush_Ghazaryan <anush93ghazaryan@gmail.com>
2018-08-14 15:18:11 +0400

    In Progress

--- 9_13.08/GsonAPI/app/src/main/java/com/example/anush/gsonapi/MainActivity.java
+++ 9_13.08/GsonAPI/app/src/main/java/com/example/anush/gsonapi/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.anush.gsonapi;
 
 import android.app.ProgressDialog;
+import android.provider.Contacts;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,8 +29,22 @@ public class MainActivity extends AppCompatActivity {
         progressDialog = new ProgressDialog(MainActivity.this);
         progressDialog.setMessage("Loading....");
         progressDialog.show();
+        final List<UserModel> list = new ArrayList<>();
+        RetrofitInterface client = RetrofitInstance.getClient().create(RetrofitInterface.class);
+        Call<UserModel> call = client.fetchUsers();
+        call.enqueue(new Callback<UserModel>() {
+            @Override
+            public void onResponse(Call<UserModel> call, Response<UserModel> response) {
+                list = response.body();
 
+            }
 
+            @Override
+            public void onFailure(Call<UserModel> call, Throwable t) {
 
+            }
+        });
     }
+
+
 }
--- 9_13.08/GsonAPI/app/src/main/java/com/example/anush/gsonapi/RetrofitInstance.java
+++ 9_13.08/GsonAPI/app/src/main/java/com/example/anush/gsonapi/RetrofitInstance.java
@@ -1,10 +1,24 @@
 package com.example.anush.gsonapi;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitInstance {
 
 
-  //  private static Retrofit retrofit;
-    private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
+  public static final String BASE_URL = "https://randomuser.me";
+  private static Retrofit retrofit = null;
+
+
+  public static Retrofit getClient() {
+    if (retrofit==null) {
+      retrofit = new Retrofit.Builder()
+              .baseUrl(BASE_URL)
+              .addConverterFactory(GsonConverterFactory.create())
+              .build();
+    }
+    return retrofit;
+  }
 
 
 }
